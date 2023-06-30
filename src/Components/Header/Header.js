02b3940425cce868f3e7ccd135d1985a2228ecd1
paulolopest <paulotarso-lopes@hotmail.com>
2023-06-30 20:39:09 -0300

    Adding left nav bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { ReactComponent as Menu } from '../../Assets/icons/menu.svg';
 import { GlobalContext } from './../../Context/GlobalContext';
 import Logo from '../../Assets/images/cabum-logo-bcfcd17b.png';
+import profileImage from '../../Assets/images/profile_ninja-c2466b7f.png';
+import { ReactComponent as UserIcon } from '../../Assets/icons/user.svg';
+import { ReactComponent as OrderIcon } from '../../Assets/icons/truck.svg';
+import { ReactComponent as FavoritesIcon } from '../../Assets/icons/heart.svg';
+import CustomButton from './../Form/CustomButton/CustomButton';
 
 const Header = () => {
-	const [rightNav, setRightNav] = React.useState(false);
+	const [leftNav, setLeftNav] = React.useState(false);
 	const { data, login, userLogout } = React.useContext(GlobalContext);
 
 	const activeRightNav = React.useCallback(() => {
-		setRightNav(!rightNav);
-	}, [rightNav]);
+		setLeftNav(!leftNav);
+	}, [leftNav]);
+
+	const onClickOutside = (event) => {
+		if (event.target === event.currentTarget) {
+			setLeftNav(false);
+		}
+	};
 
 	return (
 		<div className="headerContainer">
@@ -19,13 +30,40 @@ const Header = () => {
 				</div>
 				<img src={Logo} alt="logo"></img>
 			</div>
-			<div className={rightNav ? `header-rightNav` : `displayNone`}>
-				<ul>
-					<li>A</li>
-					<li>A</li>
-					<li>A</li>
-					<li>A</li>
-				</ul>
+			<div
+				onClick={onClickOutside}
+				className={leftNav ? `leftNav-container` : `displayNone`}
+			>
+				<div className={leftNav ? `left-nav animeLeft` : `displayNone`}>
+					<div className="lNav-profile">
+						<img src={profileImage} alt="profile" />
+						<p>Hello. {login ? data?.name : ' Login'}</p>
+					</div>
+					<div className="lNav-Links">
+						<div className="lNav-Links-card">
+							<UserIcon />
+							<p>Profile</p>
+						</div>
+						<div className="lNav-Links-card">
+							<OrderIcon />
+							<p>Orders</p>
+						</div>
+						<div className="lNav-Links-card">
+							<FavoritesIcon />
+							<p>Favorites</p>
+						</div>
+					</div>
+					<div className="lNav-buttons">
+						{login ? (
+							<button onClick={userLogout}>LOG OUT</button>
+						) : (
+							<div>
+								<button>LOG IN</button>
+								<button>REGISTER</button>
+							</div>
+						)}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
