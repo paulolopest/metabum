9dfbc4c369aac5cff20ba38592f3465e3360027c
paulolopest <paulotarso-lopes@hotmail.com>
2023-07-23 21:03:18 -0300

    Starting the home page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,65 @@
 import React from 'react';
-import MainFeed from './MainFeed/MainFeed';
+import { banners } from './../../Utils/Extra';
+import { ReactComponent as NextIcon } from '../../Assets/icons/next-svgrepo-com.svg';
+import { ReactComponent as PreviousIcon } from '../../Assets/icons/previous-svgrepo-com.svg';
+import { AnimatePresence, motion } from 'framer-motion';
+import useMeasure from 'react-use-measure';
 
 const HomePage = () => {
+	const [bannerIndex, setBannerIndex] = React.useState(0);
+	const [prev, setPrev] = React.useState(bannerIndex);
+	const [ref, { height }] = useMeasure();
+
+	const clickNext = () => {
+		if (bannerIndex === 4) {
+			setBannerIndex(0);
+			setPrev(bannerIndex);
+		} else {
+			setBannerIndex(bannerIndex + 1);
+			setPrev(bannerIndex);
+		}
+	};
+
+	const clickBack = () => {
+		bannerIndex === 4 ? setBannerIndex(0) : setBannerIndex(bannerIndex - 1);
+	};
+
+	let direction = bannerIndex > prev ? 'increasing' : 'decreasing';
+
+	console.log(direction);
+
 	return (
-		<div>
-			<MainFeed />
+		<div
+			className="homePage-Section"
+			style={{ backgroundColor: banners[bannerIndex].color }}
+		>
+			<div className="homePage-Container">
+				<motion.div
+					ref={ref}
+					className="hp-banners"
+					style={{
+						backgroundImage: `url(${banners[bannerIndex].url})`,
+					}}
+					initial={{ x: direction === 'increasing' ? 20 : -20 }}
+					animate={{ x: 0 }}
+					key={bannerIndex}
+				>
+					<button onClick={clickBack} className="hp-bannerNext">
+						<PreviousIcon />
+					</button>
+					<button onClick={clickNext} className="hp-bannerPrevious">
+						<NextIcon />
+					</button>
+				</motion.div>
+
+				<div
+					style={{
+						backgroundColor: banners[bannerIndex].color,
+						height: height,
+					}}
+				/>
+				<div>a</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/Utils/Extra.js
@@ -0,0 +1,22 @@
+export const banners = [
+	{
+		url: 'https://www.kabum.com.br/core/_next/image?url=https://themes.kabum.com.br/conteudo/layout/3591/banner_img.jpg&w=1920&h=400&q=100',
+		color: '#001a3d',
+	},
+	{
+		url: 'https://www.kabum.com.br/core/_next/image?url=https://themes.kabum.com.br/conteudo/layout/3548/banner_img.jpg&w=1920&h=400&q=100',
+		color: '#f4f4f4',
+	},
+	{
+		url: 'https://www.kabum.com.br/core/_next/image?url=https://themes.kabum.com.br/conteudo/layout/3600/banner_img.jpg&w=1920&h=400&q=100',
+		color: '#300201',
+	},
+	{
+		url: 'https://www.kabum.com.br/core/_next/image?url=https://themes.kabum.com.br/conteudo/layout/3592/banner_img.jpg&w=1920&h=400&q=100',
+		color: '#001a3d',
+	},
+	{
+		url: 'https://www.kabum.com.br/core/_next/image?url=https://themes.kabum.com.br/conteudo/layout/3427/banner_img.jpg&w=1920&h=400&q=100',
+		color: '#ff760d',
+	},
+];
